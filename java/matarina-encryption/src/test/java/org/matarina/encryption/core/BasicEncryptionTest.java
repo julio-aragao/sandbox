b4a5cb58f0e5f9390db771d11fe9957db0d109db
Julio Aragao <julio@quimbik.com>
2021-11-26 01:51:23 -0300

    initial BasicEncryptionTest

---
+++ java/matarina-encryption/src/test/java/org/matarina/encryption/core/BasicEncryptionTest.java
@@ -0,0 +1,9 @@
+package org.matarina.encryption.core;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class BasicEncryptionTest {
+    
+}
